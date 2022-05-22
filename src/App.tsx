7c8198bd12a3aec260ba765b305ddd40789168c7
Anthony Brignano <abrignano@comcast.net>
2022-05-22 15:51:54 -0400

    add splash page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import withSplashScreen from './components/Splash';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        mock home page
       </header>
     </div>
   );
 }
 
-export default App;
+export default withSplashScreen(App);
---
+++ src/components/Splash.tsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import './Splash.css';
+import { ReactComponent as Signature } from './signature.svg';
+
+function LoadingMessage() {
+    return (
+        <div>
+            <Signature/>
+        </div>
+    );
+}
+
+function withSplash(WrappedComponent: any) {
+    return class extends Component {
+        constructor(props: any) {
+            super(props);
+            this.state = {
+                loading: true,
+            };
+        }
+
+        async componentDidMount() {
+            try {
+                setTimeout(() => {
+                    this.setState({
+                        loading: false,
+                    });
+                }, 3000)
+            } catch (err) {
+                console.log(err);
+                this.setState({
+                    loading: false,
+                });
+            }
+        }
+
+        render() {
+            // while checking user session, show "loading" message
+            // @ts-ignore
+            if (this.state.loading) return LoadingMessage();
+
+            // otherwise, show the desired route
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+}
+
+export default withSplash;
